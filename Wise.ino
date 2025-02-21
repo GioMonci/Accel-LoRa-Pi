761c1c4a8977c086126457069d6e7f04dc898938
GM <95672640+GioMonci@users.noreply.github.com>
2025-02-21 12:42:23 -0500

    Add files via upload

--- Wise.ino
+++ Wise.ino
@@ -11,14 +11,35 @@
 #include "accelerometer.h"
 #include "lorawan.h"
 
+TaskHandle_t accelTaskHandle;
+TaskHandle_t loraTaskHandle;
 
-void setup(void) {
-  accelerometer_Setup();
-  lora_setup();
+// Accelerometer Task
+void accelTask(void *pvParameters) {
+    while (1) {
+        accelerometer_loop();
+        vTaskDelay(10 / portTICK_PERIOD_MS);  // Adjust delay as needed
+    }
+}
 
+// LoRaWAN Task
+void loraTask(void *pvParameters) {
+    while (1) {
+        lora_loop();
+        vTaskDelay(1000 / portTICK_PERIOD_MS);  // Adjust delay as needed
+    }
+}
+
+void setup() {
+    
+    accelerometer_Setup();
+    lora_setup();
+
+    // Create tasks
+    xTaskCreatePinnedToCore(accelTask, "AccelTask", 4096, NULL, 1, &accelTaskHandle, 0);
+    xTaskCreatePinnedToCore(loraTask, "LoraTask", 4096, NULL, 1, &loraTaskHandle, 1);
 }
 
 void loop() {
-  accelerometer_loop();
-  lora_loop();
+    vTaskDelete(NULL);  // Delete the default loop task (not needed)
 }
--- lorawan.cpp
+++ lorawan.cpp
@@ -1,3 +1,39 @@
+#include "lorawan.h"
+// This EUI must be in little-endian format, so least-significant-byte
+// first. When copying an EUI from ttnctl output, this means to reverse
+// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
+// 0x70.
+
+//JOIN EUI Little Endian form 0x45,0xba,0xe9,0x69,0x65,0xb7,0xca,0x79
+static const u1_t PROGMEM APPEUI[8]={0x45,0xba,0xe9,0x69,0x65,0xb7,0xca,0x79};
+void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+
+// LSB
+static const u1_t PROGMEM DEVEUI[8]={0x2d,0x80,0x2e,0x01,0x38,0xd7,0xb1,0x4a};
+void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+//LSB
+//static const u1_t PROGMEM APPKEY[16] = {0xeb,0x24,0xd0,0xe8,0x6d,0x6b,0xec,0xf5,0xad,0xc9,0x6f,0x1a,0x26,0xe4,0x69,0xf8};
+//MSB
+static const u1_t PROGMEM APPKEY[16] = {0xf8,0x69,0xe4,0x26,0x1a,0x6f,0xc9,0xad,0xf5,0xec,0x6b,0x6d,0xe8,0xd0,0x24,0xeb};
+
+void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+
+sensors_event_t accel;
+sensors_event_t mydata[3] = [accel.acceleration.x, accel.acceleration.y, accel.acceleration.z];
+static osjob_t sendjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 33,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 15,
+    .dio = {27},
+};
+
 void printHex2(unsigned v) {
     v &= 0xff;
     if (v < 16)
